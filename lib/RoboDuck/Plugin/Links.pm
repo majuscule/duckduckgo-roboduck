83c777b01c2c3efe15d098574e0a7234d1a2ea03
Michael Smith <crazedpsyc@duckduckgo.com>
2012-04-16 18:22:51 -0600

    add browser-specific links and organize them all

--- lib/RoboDuck/Plugin/Links.pm
+++ lib/RoboDuck/Plugin/Links.pm
@@ -4,50 +4,71 @@ use Moses::Plugin;
 use String::Trim;
 
 my %links = (
+    # duckduckgo.com/*.html / etc.
     "goodie"    => "https://duckduckgo.com/goodies.html",
     "bang"      => "https://duckduckgo.com/bang.html",
     "newbang"   => "https://duckduckgo.com/newbang.html",
     "about"     => "https://duckduckgo.com/about.html",
     "setting"   => "https://duckduckgo.com/settings.html",
+    "param"     => "https://duckduckgo.com/params.html",
     "privacy"   => "https://duckduckgo.com/privacy.html",
+    "feedback"  => "https://duckduckgo.com/feedback.html",
+    "spread"    => "https://duckduckgo.com/spread.html",
+    "traffic"   => "https://duckduckgo.com/traffic.html",
+    "tech"      => "https://duckduckgo.com/tech.html",
+    "searchbox" => "https://duckduckgo.com/search_box.html",
+    "api"       => "https://api.duckduckgo.com/",
+    "homepage"  => "https://duckduckgo.com/",
+    "home"      => \"homepage",
+    "h"         => \"homepage",
+    "shorturl"  => "http://ddg.gg/",
+
+    # minisites
     "dontbubble"=> "http://dontbubble.us/",
     "donttrack" => "http://donttrack.us/",
+
+    # other DDG sites
     "help"      => "https://help.duckduckgo.com/",
-    "feedback"  => "https://duckduckgo.com/feedback.html",
     "community" => "https://dukgo.com/",
     "forum"     => "https://duck.co/",
-    "spread"    => "https://duckduckgo.com/spread.html",
+    "duckpan"   => "http://duckpan.org/",
+
+    # social
     "twitter"   => "https://twitter.com/duckduckgo",
     "facebook"  => "https://facebook.com/duckduckgo",
     "sticker"   => "https://www.stickermule.com/duckduckgo",
-    "shorturl"  => "http://ddg.gg/",
     "github"    => "https://github.com/duckduckgo",
     "store"     => "http://cafepress.com/duckduckgo",
     "reddit"    => "http://www.reddit.com/r/duckduckgo",
     "identica"  => "https://identi.ca/duckduckgo",
     "diaspora"  => "https://joindiaspora.com/u/duckduckgo",
-    "duckpan"   => "http://duckpan.org/",
-    "homepage"  => "https://duckduckgo.com/",
-    "home"      => \"homepage",
-    "h"         => \"homepage",
-    "api"       => "https://api.duckduckgo.com/",
-    "traffic"   => "https://duckduckgo.com/traffic.html",
+
+    # official ddg articles (help.ddg, blogposts, etc)
     "browser"   => "http://help.duckduckgo.com/customer/portal/articles/216425-browsers",
     "addsite"   => "http://help.duckduckgo.com/customer/portal/articles/216407",
     "addad"     => "http://help.duckduckgo.com/customer/portal/articles/216405",
     "syntax"    => "http://help.duckduckgo.com/customer/portal/articles/300304-syntax",
     "image"     => "http://help.duckduckgo.com/customer/portal/articles/215615-images",
     "email"     => "http://help.duckduckgo.com/customer/portal/articles/215614-email",
-    "soul"      => "http://www.youtube.com/watch?v=XvwK-3cQ6gE",
-    "die"       => "http://www.youtube.com/watch?v=K5sANHYp_IQ",
     "history"   => "http://help.duckduckgo.com/customer/portal/articles/216406-history",
     "source"    => "http://help.duckduckgo.com/customer/portal/articles/216399-sources",
-    "tech"      => "https://duckduckgo.com/tech.html",
-    "pii"       => "http://www.gabrielweinberg.com/blog/2010/11/how-to-not-log-personally-identifiable-information.html",
-    "searchbox" => "https://duckduckgo.com/search_box.html",
     "spam"      => "http://help.duckduckgo.com/customer/portal/articles/215611-spam",
-    "hackhackgo"=> "http://duckduckhack.com/",
-    "hack"      => \"hackhackgo",
+    "pii"       => "http://www.gabrielweinberg.com/blog/2010/11/how-to-not-log-personally-identifiable-information.html",
+
+    # specific browsers
+    "firefox"   => "http://help.duckduckgo.com/customer/portal/articles/216441-firefox",
+    "chrome"    => "http://help.duckduckgo.com/customer/en/portal/articles/216440",
+    "opera"     => "http://help.duckduckgo.com/customer/portal/articles/216453-opera",
+    "safari"    => "http://help.duckduckgo.com/customer/portal/articles/216447-safari",
+    "android"   => "http://help.duckduckgo.com/customer/portal/articles/216461-android",
+    "ie"        => "http://help.duckduckgo.com/customer/portal/articles/216464-internet-explorer",
+    "omniweb"   => "http://help.duckduckgo.com/customer/portal/articles/216459-omniweb",
+    "epiphany"  => "http://help.duckduckgo.com/customer/portal/articles/216460-epiphany",
+    "conkeror"  => "http://help.duckduckgo.com/customer/portal/articles/216462-conkeror",
+
+    # random
+    "soul"      => "http://www.youtube.com/watch?v=XvwK-3cQ6gE",
+    "die"       => "http://www.youtube.com/watch?v=K5sANHYp_IQ",
 );
 
 sub give_link {
