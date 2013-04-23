844c9285eedf433dbc8d4b0cf53ebf4f6797c6e8
Michael Smith <crazedpsyc@lavabit.com>
2013-04-23 12:14:58 -0600

    Fix version number in DuckDuckGo useragent

--- lib/RoboDuck/Plugin/DuckDuckGo.pm
+++ lib/RoboDuck/Plugin/DuckDuckGo.pm
@@ -14,7 +14,7 @@ has ddg => (
 );
 
 sub _build_ddg {
-    WWW::DuckDuckGo->new( http_agent_name => __PACKAGE__ . '/' . "0.0development", safeoff => 1 );
+    WWW::DuckDuckGo->new( http_agent_name => __PACKAGE__ . '/' . $RoboDuck::VERSION, safeoff => 1 );
 }
 
 sub S_say_later {
