e19f9b2acad1b339be378683d01a0133d5464b0d
Michael Smith <crazedpsyc@duckduckgo.com>
2014-03-26 08:12:08 -0600

    Update links plugin

--- lib/RoboDuck/Plugin/Links.pm
+++ lib/RoboDuck/Plugin/Links.pm
@@ -2,10 +2,11 @@ package RoboDuck::Plugin::Links;
 use 5.10.0;
 use Moses::Plugin;
 use String::Trim;
+use URI::Encode 'uri_encode';
 
 my %links = (
     # duckduckgo.com/*.html / etc.
-    "goodie"    => "https://duckduckgo.com/goodies.html",
+    "goodie"    => "https://duckduckgo.com/goodies",
     "bang"      => "https://duckduckgo.com/bang.html",
     "newbang"   => "https://duckduckgo.com/newbang.html",
     "about"     => "https://duckduckgo.com/about.html",
@@ -15,7 +16,6 @@ my %links = (
     "feedback"  => "https://duckduckgo.com/feedback.html",
     "spread"    => "https://duckduckgo.com/spread.html",
     "traffic"   => "https://duckduckgo.com/traffic.html",
-    "tech"      => "https://duckduckgo.com/tech.html",
     "searchbox" => "https://duckduckgo.com/search_box.html",
     "api"       => "https://api.duckduckgo.com/",
     "homepage"  => "https://duckduckgo.com/",
@@ -31,9 +31,9 @@ my %links = (
     "ddh"       => \"duckduckhack",
 
     # other DDG sites
-    "help"      => "https://help.duckduckgo.com/",
-    "community" => "https://dukgo.com/",
-    "forum"     => "https://duck.co/",
+    "help"      => "https://duck.co/help/",
+    "community" => "https://duck.co/",
+    "forum"     => "https://duck.co/forum",
     "duckpan"   => "http://duckpan.org/",
 
     # social
@@ -47,28 +47,7 @@ my %links = (
     "diaspora"  => "https://joindiaspora.com/u/duckduckgo",
 
     # official ddg articles (help.ddg, blogposts, etc)
-    "browser"   => "http://help.duckduckgo.com/customer/portal/articles/216425-browsers",
-    "addsite"   => "http://help.duckduckgo.com/customer/portal/articles/216407",
-    "addad"     => "http://help.duckduckgo.com/customer/portal/articles/216405",
-    "syntax"    => "http://help.duckduckgo.com/customer/portal/articles/300304-syntax",
-    "image"     => "http://help.duckduckgo.com/customer/portal/articles/215615-images",
-    "email"     => "http://help.duckduckgo.com/customer/portal/articles/215614-email",
-    "history"   => "http://help.duckduckgo.com/customer/portal/articles/216406-history",
-    "source"    => "http://help.duckduckgo.com/customer/portal/articles/216399-sources",
-    "spam"      => "http://help.duckduckgo.com/customer/portal/articles/215611-spam",
-    "logo"      => "http://help.duckduckgo.com/customer/portal/articles/485613-holiday-logos",
     "pii"       => "http://www.gabrielweinberg.com/blog/2010/11/how-to-not-log-personally-identifiable-information.html",
-
-    # specific browsers
-    "firefox"   => "http://help.duckduckgo.com/customer/portal/articles/216441-firefox",
-    "chrome"    => "http://help.duckduckgo.com/customer/en/portal/articles/216440",
-    "opera"     => "http://help.duckduckgo.com/customer/portal/articles/216453-opera",
-    "safari"    => "http://help.duckduckgo.com/customer/portal/articles/216447-safari",
-    "android"   => "http://help.duckduckgo.com/customer/portal/articles/216461-android",
-    "ie"        => "http://help.duckduckgo.com/customer/portal/articles/216464-internet-explorer",
-    "omniweb"   => "http://help.duckduckgo.com/customer/portal/articles/216459-omniweb",
-    "epiphany"  => "http://help.duckduckgo.com/customer/portal/articles/216460-epiphany",
-    "conkeror"  => "http://help.duckduckgo.com/customer/portal/articles/216462-conkeror",
 
     # random
     "soul"      => "http://www.youtube.com/watch?v=XvwK-3cQ6gE",
@@ -85,7 +64,7 @@ sub give_link {
         my $value = $links{$key};
         $reply = ref $value eq 'SCALAR' ? $links{$$value} : $value;
     } else {
-        $reply = "I don't have a link to \"$key\"";
+        $reply = "https://duck.co/help/search?help_search=".uri_encode($key)."&ducky=1";
     }
     $self->privmsg( $_ => "$target: ".$reply ) for @$$channels;
 }
